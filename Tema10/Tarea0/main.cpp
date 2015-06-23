4b1219de717e37701fa5e7d3df42ec704db47260
Juan Garcia Montalvillo <montalvillo@outlook.com>
2015-06-23 20:24:12 +0200

    Añadido Tema 10

---
+++ Tema10/Tarea0/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "puntopantallah.h"
+
+using namespace std;
+
+int main()
+{
+    PuntoPantalla miPuntoxy(10,30);
+
+    cout <<"Punto x " <<miPuntoxy.getx()<< endl;
+
+    cout <<"Punto y "<<miPuntoxy.gety();
+    return 0;
+}
