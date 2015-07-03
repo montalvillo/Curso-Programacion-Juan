2b939191199362eae1f7e23c1cd5a3c3d6968019
Juan Garcia Montalvillo <montalvillo@outlook.com>
2015-07-03 18:40:39 +0200

    Tema11Tarea02
    Añadida la tarea

---
+++ Tema11/Tema11Tarea02/main.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int x;
+
+  do {
+
+    cout << ("Introduce");
+    cin >>x;
+
+    if (x%2==0){
+
+        cout <<x<<" Soy par"  << endl;
+    }
+
+}
+while(x!=44 && x!=99);
+
+
+  return 0;
+}
