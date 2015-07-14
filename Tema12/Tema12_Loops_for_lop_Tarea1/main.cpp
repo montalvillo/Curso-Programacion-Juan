4477be174092674c5986a1b307358d067a920944
Juan Garcia Montalvillo <montalvillo@outlook.com>
2015-07-14 17:51:13 +0200

    Tema12_Loops_For_Loop_Tarea1
    Realizada Tarea 1

---
+++ Tema12/Tema12_Loops_for_lop_Tarea1/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	for( int i = 1 , j = 100; i >= 100, j >= 1; i++, j--){
+
+
+
+    cout <<i << "\t "<< j<<endl;}
+
+
+
+
+return 0;}
+
+
+
+ /**<  Piramide
+ int main ()
+  { int space; int rows; int star;
+  for (rows = 0; rows <9; rows ++)
+{ for (space = 0; space < 8; space ++)
+{ cout << "*" ; } cout << " " ; } system ( "pause" ); return 0; } */
+
+
+ /**< Piramide  int main()
+  { int i, j = 1; for (i=1; i<9; i++)
+   { cout << j; j = j*2; } system( "pause" ); return 0; } */
+
+
+/**< Piramide
+int main()
+ {
+int i, j = 1, y;
+for (i=1; i<3; i++)
+    { for (y=1; y<5; y++)
+     { cout << j; j = j*2; } cout << endl; } system( "pause" );
+return 0; } */
+
