39c73483c40e2875af9e4260e1219d3cbad1c7d4
Juan Garcia Montalvillo <montalvillo@outlook.com>
2015-08-07 18:26:27 +0200

    Tema12_Loops_For_LoopTarea02
    Terminado

---
+++ Tema12/Tema12_Loops_For_LoopTarea02/main.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+
+using namespace std;
+
+
+    int main()
+{
+
+   int n;
+  cout<<"Introduce el numero: "; cin>>n;
+
+for( int a=1;a<=n;a++){
+cout<<a<<endl;}
+for (int b=1;b<=n;b+=2){
+
+cout <<"\t"<<b <<endl;}
+for (int c =1;c<=n;c+=3){
+
+cout<<"\t"<<"\t"<<c<< endl;}
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**<  int main(){
+
+int i,y;
+
+    for (i = 1;i<=3; i++)
+
+
+        {for (y=1;y<=4;y++)
+
+
+
+cout << "\t"<<y<<i;
+
+
+       cout <<("\n");}
+
+
+
+return 0;
+        }
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**<  int main()
+{
+
+   int n;
+  cout<<"Introduce el numero: "; cin>>n;
+  for(int a=1;a<=n;a++)
+
+cout << a<< endl;
+
+
+for (int b=1;b<=n;b+=2)
+
+
+   cout <<"\t"<<b <<endl;
+
+
+for (int c =1;c<=n;c+=3)
+cout<<"\t" <<"\n"<<c<< endl;
+
+return 0;
+}
+*/
