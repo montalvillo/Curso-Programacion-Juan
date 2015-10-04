3842bfdd6649f7ef9f2f076df6094bde83e06289
Juan Garcia Montalvillo <montalvillo@outlook.com>
2015-10-04 20:44:12 +0200

    Tarea 0
    Corregido

--- Tema16/Tema16_Punteros y memoria dimanica_Tarea_0/main.cpp
+++ Tema16/Tema16_Punteros y memoria dimanica_Tarea_0/main.cpp
@@ -5,35 +5,46 @@ using namespace std;
 
 int main()
 {
-    char  *caracter = new char('A') ;
+    char  *caracter;
+
+    caracter = new char('A') ;
 
     cout<<"Yo soy el caracter "<< *caracter<<endl;
 
     delete caracter;
 
-    int  *entero = new int (5);
+    int  *entero ;
+
+    entero = new int (5);
 
     cout<<"Yo soy el entero y valgo "<< *entero<<endl;
 
     delete entero;
 
-    float *decimal = new float (2.2);
+    float *decimal;
+
+    decimal = new float (2.2);
 
     cout<<"Yo soy el float y valgo "<< *decimal<<endl;
 
     delete decimal;
 
-    double *decimallargo = new double (4.44444);
+    double *decimallargo ;
+
+    decimallargo= new double (4.44444);
 
     cout<<"Yo soy el double y valgo "<< *decimallargo<<endl;
 
-    long *largo = new long (1234);
+    long *largo;
+
+    largo = new long (1234);
 
     cout<<"Yo soy el long y valgo "<< *largo<<endl;
 
     delete largo;
 
-    bool *deci = new bool (false);
+    bool *deci;
+    deci = new bool (false);
 
 
     if (*deci){
